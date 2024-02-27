7d80bfd52be35922bfb59a57a470d4183c9b3a4c
kassymm <k.mukhanbetiyar@gmail.com>
2024-02-27 17:13:03 +0800

    updated the parser
    now you can stop the parser and continue the process later

--- predictions.py
+++ predictions.py
@@ -16,7 +16,7 @@ base_directory = '/Volumes/Transcend/Verigram/CelebA/archive/CelebA_Spoof_/Celeb
 total_rows = len(df)
 progress_interval = 250
 processed_count = 0
-checkpoint_file = 'progress_checkpoint.csv'
+checkpoint_file = 'dataset.csv'
 
 # Check if a checkpoint file exists
 if os.path.exists(checkpoint_file):
@@ -25,6 +25,7 @@ if os.path.exists(checkpoint_file):
     df.update(checkpoint_df)
 
 start_time = time.time()
+last_interval_start_time = start_time
 
 for row_name in df.index[processed_count:]:
     image_path = os.path.join(base_directory, row_name)
@@ -33,18 +34,14 @@ for row_name in df.index[processed_count:]:
     df.loc[row_name, 'Prediction Score'] = value
     processed_count += 1
     if processed_count % progress_interval == 0 or processed_count == total_rows:
-        elapsed_time = time.time() - start_time
-        average_time_per_sample = elapsed_time / processed_count
+        elapsed_time = time.time() - last_interval_start_time
+        last_interval_start_time = time.time()
+        average_time_per_sample = elapsed_time / progress_interval
         estimated_remaining_time = average_time_per_sample * (total_rows - processed_count)
         print(f"Processed {processed_count}/{total_rows} samples. Elapsed time: {elapsed_time:.2f}s. Estimated remaining time: {estimated_remaining_time:.2f}s")
-        # Save the DataFrame as a checkpoint
-        df.iloc[:processed_count].to_csv(checkpoint_file)
-
-# Remove the checkpoint file
-if os.path.exists(checkpoint_file):
-    os.remove(checkpoint_file)
+        # Save the DataFrame as a checkpoint (append mode)
+        df.iloc[processed_count - progress_interval:processed_count].to_csv(checkpoint_file, mode='a', header=False)
 
 # Calculate the elapsed time
 elapsed_time = time.time() - start_time
 print(f"Total time taken: {elapsed_time:.2f}s")
-
